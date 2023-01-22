b1946db97be8d14e603ae10ebe7a473764850874
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2023-01-22 14:37:01 +0100

    Ajout de l'entité et du RepositoryRestRessource

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,35 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.saveAll(
+                    List.of(
+                            Customer.builder().nom("Hassane").email("hass@gmail.com").build(),
+                            Customer.builder().nom("Julie").email("julgmail.com").build(),
+                            Customer.builder().nom("Moussa").email("mousgmail.com").build()
+                    )
+            );
+            customerRepository.findAll().forEach(customer -> {
+                System.out.println(customer);
+            });
+        };
     }
 
 }
