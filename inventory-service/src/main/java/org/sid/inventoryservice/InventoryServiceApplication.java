123e9dc258f63ec7ce6febc68ed40434eab94754
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2023-01-22 15:35:43 +0100

    Ajout de l'entité et du RepositoryRestRessource du deuxième module

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,32 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import java.util.List;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+        return args -> {
+            restConfiguration.exposeIdsFor(Product.class);
+            productRepository.saveAll(
+                    List.of(Product.builder().name("Computer").price(12).quantity(1200).build(),
+                            Product.builder().name("Printer").price(32).quantity(120).build(),
+                            Product.builder().name("Smartphone").price(31).quantity(900).build())
+            );
+        };
     }
 
 }
