29cf3aa2719cd01b8818afd0ada94816e13a1ca8
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2023-01-22 16:18:49 +0100

    Configuration du gateway de manière statique en utilisant une classe de config

--- gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -2,12 +2,23 @@ package org.sid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/customers/**").uri("http://localhost:9091"))
+                .route(r->r.path("/products/**").uri("http://localhost:9092")).build();
     }
 
 }
