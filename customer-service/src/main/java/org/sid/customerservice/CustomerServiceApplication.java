b2e17e2b5d729bbf7f4350c12da77d9c8507d779
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2023-01-22 14:44:46 +0100

    Ajout de restConfiguration pour exposer l'id

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.List;
 
@@ -17,8 +18,11 @@ public class CustomerServiceApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+    public CommandLineRunner commandLineRunner(CustomerRepository customerRepository,
+                                               RepositoryRestConfiguration restConfiguration){
         return args -> {
+            //Par defaut RestRessource n'expose pas l'id donc ajouté RestConfiguration pour exposer l'id
+            restConfiguration.exposeIdsFor(Customer.class);
             customerRepository.saveAll(
                     List.of(
                             Customer.builder().nom("Hassane").email("hass@gmail.com").build(),
