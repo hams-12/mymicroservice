b3420e7abd88c4e7c72263b5379fd751794c8882
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2023-01-23 08:17:38 +0100

    Ajout d\'un controller pour voir une facture complète(avec infos client et info produits aulieu des id seulement

--- billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
+++ billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -1,5 +1,6 @@
 package org.sid.billingservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class ProductItem {
     private Long id;
     private Long productId; //puisque product ne se trouve pas dans la même BD on déclare de cette manière avec la cle etrangère
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //Pour ne pas tomber dans la bouble infinie. Quand tu vas me retourner un product item ce n'est pas la peine de me retourner bill
     private Bill bill;
     private int quantity; //cette quantité represente la qté commandée du produit dans la facture
     private double price;
---
+++ billing-service/src/main/java/org/sid/billingservice/web/BillRestController.java
@@ -0,0 +1,34 @@
+package org.sid.billingservice.web;
+
+import org.sid.billingservice.entities.Bill;
+import org.sid.billingservice.repositories.BillRepository;
+import org.sid.billingservice.repositories.ProductItemRepository;
+import org.sid.billingservice.service.CustomerRestClient;
+import org.sid.billingservice.service.ProductRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BillRestController {
+    @Autowired
+    private BillRepository billRepository;
+    @Autowired
+    private ProductItemRepository productItemRepository;
+    @Autowired
+    private CustomerRestClient customerRestClient;
+    @Autowired
+    private ProductRestClient productRestClient;
+
+    @GetMapping(path = "/fullbill/{id}")
+    public Bill bill(@PathVariable Long id){
+        Bill billl = billRepository.findById(id).get(); //La facture que je recupère ici contient uniquement l'id du customer mais pas toutes ces informations
+        billl.setCustomer(customerRestClient.findCustomerById(billl.getCustomerId()));
+        billl.getProductItems().forEach(pi->{
+            pi.setProduct(productRestClient.findProductById(pi.getProductId()));
+        });
+        return billl;
+    }
+
+}
